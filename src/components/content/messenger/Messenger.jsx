90beb2347b1eb507dc922646c658b84e8e05e807
Worsto <worsto.inbox@gmail.com>
2021-01-27 13:09:15 +0300

    change place

--- src/components/content/messenger/Messenger.jsx
+++ src/components/content/messenger/Messenger.jsx
@@ -8,6 +8,13 @@ const Messenger = (props) => {
   const chatsElements = props.data.chats.map( ch => <ChatItem name={ch.name} id={ch.id} />)
   const massagesElements = props.data.messages.map( me => <ChatMessage text={me.text} id={me.id} /> )
 
+  let newMessage = React.createRef();
+
+  const sendMessage = () => {
+    let message = newMessage.current.value;
+    alert(message)
+  }
+
   return (
       <section className={styles.messenger}>
         <h2 className='visually-hidden'>Messenger</h2>
@@ -17,6 +24,10 @@ const Messenger = (props) => {
         <ul className={styles.chat}>
           {massagesElements}
         </ul>
+        <form action="">
+          <textarea ref={newMessage}></textarea>
+          <button onClick={sendMessage}>Send</button>
+        </form>
       </section>
   )
 }
